0efb2b9ae86a552fd1133c5955921637bfd4819c
mhaajunior <junior29958@gmail.com>
2023-10-18 15:02:55 +0700

    update cookie

--- src/helpers/user.helper.ts
+++ src/helpers/user.helper.ts
@@ -7,7 +7,7 @@ export const generateToken = (res: Response, _id: ObjectId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
     sameSite: "strict",
     maxAge: 3 * 24 * 60 * 60 * 1000,
   });
